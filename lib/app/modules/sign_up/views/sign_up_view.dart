4aca63e5abb39834b554a762cc1f60519f244c37
Sashisaha <sashisaha98@gmail.com>
2025-12-03 15:42:09 +0600

    miyam pull

--- lib/app/modules/sign_up/views/sign_up_view.dart
+++ lib/app/modules/sign_up/views/sign_up_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:my_ecommerce/app/data/app_text_styles.dart';
+import '../../all_common_widgets/custom_textformfield.dart';
 import '../controllers/sign_up_controller.dart';
 
 class SignUpView extends GetView<SignUpController> {
@@ -23,25 +24,28 @@ class SignUpView extends GetView<SignUpController> {
               ),
               SizedBox(height: 24.h),
               Text("Full Name", style: AppTextStyles.medium16),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter your full name",
-                  suffix: Icon(
-                    Icons.remove_red_eye_outlined,
-                  ),
-                ),
+              CustomTextformfield(text: "Enter your full name"),
+              SizedBox(height: 16.h),
+              Text("Email", style: AppTextStyles.medium16),
+              CustomTextformfield(text: "Enter your email address"),
+              SizedBox(height: 16.h),
+              Text("Password", style: AppTextStyles.medium16),
+              CustomTextformfield(
+                text: "Enter your password",
+                icon: Icons.remove_red_eye_outlined,
               ),
-              SizedBox(height: 16.h,),
-              Text("Email",style: AppTextStyles.medium16,),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter your full name",
-                  suffix: Icon(
-                    Icons.remove_red_eye_outlined,
-                  ),
-                ),
-              ),
+              SizedBox(height: 16.h),
+              RichText(
+                text: TextSpan(
+                  text: "By signing up you agree to our",
+                  style: AppTextStyles.regular14,
+                  children: <TextSpan>[
+                    TextSpan(text: "Terms, Privacy Policy,",style: AppTextStyles.bold14),
+                    TextSpan(text: "and,",style: AppTextStyles.bold14),
 
+                  ],
+                ),
+              ),
             ],
           ),
         ),
